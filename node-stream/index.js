1da862feaf2f97610acfe6bd598ee03270ad0127
zhangkaidi <490151956@qq.com>
2018-01-05 11:01:10 +0800

    node-streamImg

--- node-stream/index.js
+++ node-stream/index.js
@@ -1,4 +1,23 @@
-var url = 'http://img.taopic.com/uploads/allimg/120727/201995-120HG1030762.jpg';
-var request = require('request'),
-fs = require('fs');
-request(url).pipe(fs.createWriteStream('test.jpg'));
+var fs = require('fs');
+var cheerio = require('cheerio');
+var http = require('http');
+var request = require('request');
+
+fs.readFile('herolist.json', 'utf-8',function (err, data) {
+	if (err) {
+		return console.error(err);
+		}else{
+			var data = JSON.parse(data),
+			img_filename,
+			_ename,
+			imgUrl;
+			data.forEach(function(item, index){
+				_ename = item.ename,
+				_cname = item.cname, 
+				img_filename= _cname + '.jpg',
+				imgUrl = 'http://game.gtimg.cn/images/yxzj/img201606/heroimg/'+_ename+'/'+_ename+'.jpg';
+				request(imgUrl).pipe(fs.createWriteStream('./image/'+img_filename));
+			})
+		}
+});
+
